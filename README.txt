a40a4ffd0a44cc061c72aa1a7225997fa6ad1cb8
Matthew McClean <matt.mcclean@gmail.com>
2012-03-08 08:35:06 +0400

    Added LICENSE, setup.py files

---
+++ README.txt
@@ -0,0 +1,7 @@
+============
+Salt Monitor
+============
+
+Salt Monitor provides a monitoring and alterting framework based
+on the SaltStack remote execution project http://saltstack.org.
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SaltMonitor',
+    version='0.1dev',
+    packages=['saltmonitor',],
+    license='Apache License 2.0',
+    long_description=open('README.txt').read(),
+)
